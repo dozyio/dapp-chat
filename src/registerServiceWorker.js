a20c95712dcfa3da6bb3abf9ece1365e4bd02f0c
dozy <github@dozy.io>
2020-08-06 13:56:00 +0100

    deps update

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,7 +2,7 @@
 import { register } from 'register-service-worker';
 
 if (process.env.NODE_ENV === 'production') {
-    if(navigator.serviceWorkder){
+    if(navigator.serviceWorker){
         register(`${process.env.BASE_URL}service-worker.js`, {
             ready () {
                 console.log('Service worker is active.');
